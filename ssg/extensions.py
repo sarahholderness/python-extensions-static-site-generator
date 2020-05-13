c915c57549d851ff0a871dac697e7354ff4573fe
sarah <sarah-holderness@pluralsight.com>
2020-05-13 12:45:46 -0400

    Tasks 5-6

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -10,3 +10,8 @@ def load_module(directory, name):
 def load_directory(directory):
     for path in directory.rglob("*.py"):
         load_module(directory.as_posix(), path.stem)
+        
+
+def load_bundled():
+    directory = Path(__file__).parent / "extensions"
+    load_directory(directory)
