18cfe0c8bf476045674561f7c4e6d6921f16edc6
sarah <sarah-holderness@pluralsight.com>
2020-05-13 11:44:03 -0400

    Tasks 3-4

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -4,3 +4,9 @@ from pathlib import Path
 
 def load_module(directory, name):
     sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
+
+def load_directory(directory):
+    for path in directory.rglob("*.py"):
+        load_module(directory.as_posix(), path.stem)
