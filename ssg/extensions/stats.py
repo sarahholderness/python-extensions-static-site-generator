8035c9b08cfae3216c597525d2bd2787e748abf8
sarah <sarah-holderness@pluralsight.com>
2020-05-15 13:36:09 -0400

    Mod 4 Tasks 1-2

---
+++ ssg/extensions/stats.py
@@ -0,0 +1,11 @@
+from ssg import hooks
+import time
+
+
+start_time = None
+total_written = 0
+
+@hooks.register("start_build")
+def start_build():
+    global start_time
+    start_time = time.time()
